a5906f4915c05dacae5e1a7e6c03ebcc6cfeb5fe
Anubhav Chawla <anubhavchawla02@gmail.com>
2023-08-06 19:15:15 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,jsonify
+from models.gpt2_large import generate_gpt2_large
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/members",methods=["POST"])
+def members():
+    if request.method == "POST":
+        try:
+            data = request.get_json()
+            input_text = data.get("input_text")
+
+            # Use the generate_paragraph function from text_generator.py
+            generated_text = generate_gpt2_large(input_text)
+
+            # Create the response JSON
+            response_data = {"generated_text": generated_text}
+
+            return jsonify(response_data)
+        except Exception as e:
+            # Log the error for debugging purposes
+            print("Error processing JSON data:", e)
+            return jsonify({"error": "Invalid JSON data"}), 400
+if __name__=="__main__":
+    app.run(debug=True)
